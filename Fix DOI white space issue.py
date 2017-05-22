ca54752d94a53d25d09905c1a1221da75f8fa917
Sky <skybristol@gmail.com>
2017-05-22 09:44:13 -0600

    Several changes and new notebook pulling FWS information from TIR

--- Fix DOI white space issue.py
+++ Fix DOI white space issue.py
@@ -1,26 +1,32 @@
 
 # coding: utf-8
 
-# In[9]:
+# This is one-time code that fixes an issue in the DOIs that were minted and recorded in the GAP species model items in ScienceBase. These are the foundational metadata containers that provide information about the GAP models and their access points. The original script that generated the items from a spreadsheet inventory used a method that posted the information to a web app to mint the DOI. That app returned some whitespace characters trailing the DOI that got inserted into ScienceBase. This code strips the newline and whitespace to clean up the string and puts the identifiers list back to the ScienceBase items.
+
+# In[1]:
 
 import pysb,time
 from IPython.display import display
 
 
-# In[3]:
+# In[2]:
 
 sb = pysb.SbSession()
 username = input("Username: ")
 sb.loginc(str(username))
 
 
-# In[10]:
+# In[30]:
 
-habitatMapCollectionID = "527d0a83e4b0850ea0518326"
+#habitatMapCollectionID = "527d0a83e4b0850ea0518326"
+#collectionIDs = sb.get_child_ids(habitatMapCollectionID)
 
-collectionIDs = sb.get_child_ids(habitatMapCollectionID)
+items = sb.find_items('filter=dateRange!%3D%7B"choice"%3A"day"%7D&parentId=527d0a83e4b0850ea0518326&max=100')
 
-for item in collectionIDs:
+#for item in collectionIDs:
+for item in items["items"]:
+    item = item["id"]
+
     thisItem = sb.get_item(item)
     thisItemUpdate = {}
     thisItemUpdate["id"] = item
