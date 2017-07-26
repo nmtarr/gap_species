9667705ddbaf1377f5232084bc04e92ecedeac1c
Sky Bristol <sbristol@usgs.gov>
2017-07-26 07:49:13 -0600

    Fixed and re-ran GAP species registration process
    Also includes new reports on GAP species from TIR

--- Register GAP Species in TIR.py
+++ Register GAP Species in TIR.py
@@ -1,67 +1,42 @@
 
 # coding: utf-8
 
-# In[20]:
+# In[4]:
 
-import requests,configparser,pysb,time,datetime
+import requests
+import pysb
+import json
 from IPython.display import display
-import pandas as pd
+from bis2 import gc2
+from bis import tir
+from bis import gap
 
 
-# In[21]:
-
-# Get API keys and any other config details from a file that is external to the code.
-config = configparser.RawConfigParser()
-config.read_file(open(r'../config/stuff.py'))
-
-
-# In[22]:
-
-# Build base URL with API key using input from the external config.
-def getBaseURL():
-    gc2APIKey = config.get('apiKeys','apiKey_GC2_BCB').replace('"','')
-    apiBaseURL = "https://gc2.mapcentia.com/api/v1/sql/bcb?key="+gc2APIKey
-    return apiBaseURL
-
-
-# In[26]:
-
-# Basic function to insert registration info pairs into TIR
-def registerToTIR(recordInfoPairs):
-    # Build query string
-    insertSQL = "INSERT INTO tir.tir2 (registration) VALUES ('"+recordInfoPairs+"')"
-    # Execute query
-    response = requests.get(getBaseURL()+"&q="+insertSQL).json()
-    return response
-
-
-# In[24]:
+# In[ ]:
 
 sb = pysb.SbSession()
 username = input("Username: ")
 sb.loginc(str(username))
 
 
-# In[27]:
+# In[3]:
 
-habitatMapCollectionID = "527d0a83e4b0850ea0518326"
+# Set up the actions/targets for this particular instance
+thisRun = {}
+thisRun["instance"] = "DataDistillery"
+thisRun["db"] = "BCB"
+thisRun["baseURL"] = gc2.sqlAPI(thisRun["instance"],thisRun["db"])
 
-collectionIDs = sb.get_child_ids(habitatMapCollectionID)
+_habitatMapCollectionID = "527d0a83e4b0850ea0518326"
+_gapSpecies = sb.get_child_ids(_habitatMapCollectionID)
 
-for item in collectionIDs:
-    thisItem = sb.get_item(item)
-    recordInfoPairs = '"registrationDate"=>"'+datetime.datetime.utcnow().isoformat()+'"'
-    recordInfoPairs = recordInfoPairs+',"ScienceBaseItemID"=>"'+item+'"'
-    for identifier in thisItem["identifiers"]:
-        recordInfoPairs = recordInfoPairs+',"'+identifier["type"]+'"=>"'+identifier["key"].strip()+'"'
-    for tag in thisItem["tags"]:
-        if tag["scheme"] == "https://www.sciencebase.gov/vocab/bis/tir/scientificname":
-            recordInfoPairs = recordInfoPairs+',"GAP_ScientificName"=>"'+tag["name"]+'"'
-        elif tag["scheme"] == "https://www.sciencebase.gov/vocab/bis/tir/commonname":
-            recordInfoPairs = recordInfoPairs+',"GAP_CommonName"=>"'+tag["name"]+'"'
-        else:
-            pass
-    print (registerToTIR(recordInfoPairs))
+for item in _gapSpecies:
+    print (item)
+    checkTIR = requests.get(thisRun["baseURL"]+"&q=SELECT id FROM tir.tir WHERE registration->>'id' = '"+item+"'").json()
+    if len(checkTIR["features"]) == 0:
+        gapSpecies = gap.gapToTIR(sb.get_item(item, {'fields':'identifiers,tags'}))
+        display (gapSpecies)
+        print (tir.tirRegistration(gc2.sqlAPI("DataDistillery","BCB"),json.dumps(gapSpecies)))
 
 
 # In[ ]:
